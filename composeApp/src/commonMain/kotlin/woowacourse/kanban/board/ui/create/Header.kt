8ad1d02c61674305596bcd33a843c9fb7403053a
parkhyomi <phyomi3@naver.com>
2026-03-12 16:19:32 +0900

    feat: Dialog Header 및 Footer 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/create/Header.kt
@@ -0,0 +1,48 @@
+package woowacourse.kanban.board.ui.create
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Header(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 24.dp, vertical = 28.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = "새 태스크 생성",
+        )
+        IconButton(
+            onClick = onDismiss,
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "테스크 종료",
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun HeaderPreview() {
+    Header(
+        onDismiss = {},
+    )
+}
