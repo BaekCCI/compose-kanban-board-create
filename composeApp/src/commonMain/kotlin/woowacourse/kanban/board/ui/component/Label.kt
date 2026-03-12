948c9c5884a71b2b76ace1acee5e9cca49ff74df
parkhyomi <phyomi3@naver.com>
2026-03-12 16:12:39 +0900

    feat: 공동 라벨 컴포넌트 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/component/Label.kt
@@ -0,0 +1,38 @@
+package woowacourse.kanban.board.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.ui.theme.Gray700
+
+@Composable
+fun Label(
+    label: String,
+    required: Boolean = false,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = label,
+            color = Gray700,
+        )
+        if (required) {
+            Text(
+                text = "*",
+                color = Gray700,
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun LabelPreview() {
+    Label("로미", true)
+}
