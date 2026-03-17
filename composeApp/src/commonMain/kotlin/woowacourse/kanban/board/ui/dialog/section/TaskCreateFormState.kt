e4cc6988f9ba61971ff7282bf440bad071c19e67
baekcii <bsy8470@naver.com>
2026-03-17 22:24:05 +0900

    feat: 테스트 생성 상태 관리를 위한 TaskCreateFormState 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/dialog/section/TaskCreateFormState.kt
@@ -0,0 +1,42 @@
+package woowacourse.kanban.board.ui.dialog.section
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import woowacourse.kanban.board.domain.model.Status
+import woowacourse.kanban.board.domain.model.User
+import woowacourse.kanban.board.domain.validator.TaskValidator
+import woowacourse.kanban.board.domain.validator.ValidationResult
+
+class TaskCreateFormState(val assignees: List<User>) {
+    var title by mutableStateOf("")
+    var titleValidation: ValidationResult by mutableStateOf(ValidationResult.Initial)
+
+    var content by mutableStateOf("")
+
+    var tag by mutableStateOf("")
+    var tagValidation: ValidationResult by mutableStateOf(ValidationResult.Initial)
+
+    var selectedStatus by mutableStateOf(Status.TODO)
+    var selectedAssignee by mutableStateOf(assignees.firstOrNull())
+
+    val canCreate get() = titleValidation is ValidationResult.Valid && tagValidation !is ValidationResult.Invalid
+
+    fun updateTitle(input: String) {
+        title = input
+        titleValidation = TaskValidator.validateTitle(input)
+    }
+
+    fun updateTag(input: String) {
+        tag = input
+        tagValidation = TaskValidator.validateTags(input)
+    }
+
+    fun updateAssignee(user: User) {
+        selectedAssignee = user
+    }
+
+    fun updateStatus(status: Status) {
+        this.selectedStatus = status
+    }
+}
