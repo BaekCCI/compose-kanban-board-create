57aa5124183ac8e599a32d7385f371115204d188
baekcii <bsy8470@naver.com>
2026-03-22 03:18:53 +0900

    feat: 칸반 보드 구현

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/board/KanbanBoard.kt
@@ -0,0 +1,57 @@
+package woowacourse.kanban.board.ui.board
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import woowacourse.kanban.board.domain.model.Status
+import woowacourse.kanban.board.domain.model.Task
+
+@Composable
+fun KanbanBoard(
+    modifier: Modifier = Modifier,
+    tasks: List<Task> = emptyList(),
+    onClickCreate: () -> Unit = {},
+) {
+    val totalCount = tasks.size
+    val completeCount = tasks.filter { it.status == Status.DONE }.size
+    val completeRatio = tasks.filter { it.status == Status.DONE }.size.toFloat() / tasks.size.toFloat()
+    Column(
+        modifier = modifier.fillMaxWidth().fillMaxHeight().background(Color(0xffF9FAFB)),
+    ) {
+        KanbanHeader(
+            onClickCreate = onClickCreate,
+            totalCount = totalCount,
+            completeCount = completeCount,
+            completeRatio = completeRatio,
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(24.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Status.entries.forEach { status ->
+                TaskBox(
+                    modifier = Modifier.weight(1f, fill = false).widthIn(max = 320.dp).fillMaxHeight(),
+                    status = status,
+                    tasks = tasks.filter { it.status == status },
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 800)
+@Composable
+fun KanbanBoardPreview() {
+    KanbanBoard()
+}
