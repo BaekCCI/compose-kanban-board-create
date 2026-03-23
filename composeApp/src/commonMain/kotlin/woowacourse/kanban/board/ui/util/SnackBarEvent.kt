b742d8243c9219b39db89b61a36ca942643e354b
baekcii <bsy8470@naver.com>
2026-03-23 17:15:00 +0900

    feat: snackBarEvent 데이터 클래스 추가

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/util/SnackBarEvent.kt
@@ -0,0 +1,9 @@
+package woowacourse.kanban.board.ui.util
+
+import org.jetbrains.compose.resources.StringResource
+
+data class SnackBarEvent(
+    val id: Long = System.currentTimeMillis(),
+    val strRes: StringResource? = null,
+    val message: String? = null,
+)
