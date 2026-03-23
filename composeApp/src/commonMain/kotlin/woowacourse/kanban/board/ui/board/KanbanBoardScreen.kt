320a830393e74cccde9db190c4a8cd6689b45f98
baekcii <bsy8470@naver.com>
2026-03-23 16:11:21 +0900

    refactor: LaunchedEffect를 활용하여 스낵바 관리

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/board/KanbanBoardScreen.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/board/KanbanBoardScreen.kt
@@ -4,14 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.launch
 import woowacourse.kanban.board.domain.TaskCreator
 import woowacourse.kanban.board.ui.dialog.TaskCreateDialog
 
@@ -20,23 +19,29 @@ fun KanbanBoardScreen() {
     val boardState = remember { TaskBoardState() }
     var showDialog by remember { mutableStateOf(false) }
     val snackBarHostState = remember { SnackbarHostState() }
-    val coroutineScope = rememberCoroutineScope()
+    var snackBarMessage: String? by remember { mutableStateOf(null) }
+
+    LaunchedEffect(snackBarMessage) {
+        snackBarMessage?.let {
+            snackBarHostState.showSnackbar(message = it, withDismissAction = true)
+        }
+        snackBarMessage = null
+    }
 
     Box {
         if (showDialog) {
             TaskCreateDialog(
                 onDismissRequest = { showDialog = false },
                 onConfirm = { title, description, tags, status, assignee ->
-
                     val result =
                         TaskCreator.create(title = title, description = description, tags = tags, assignee = assignee, status = status)
 
                     result.onSuccess { newTask ->
                         boardState.createTask(newTask)
                         showDialog = false
-                        coroutineScope.launch { snackBarHostState.showSnackbar("새로운 태스크가 추가되었습니다.") }
+                        snackBarMessage = "새로운 태스크가 추가되었습니다."
                     }.onFailure { exception ->
-                        coroutineScope.launch { snackBarHostState.showSnackbar(exception.message ?: "오류 발생") }
+                        snackBarMessage = exception.message ?: "오류 발생"
                     }
                 },
             )
