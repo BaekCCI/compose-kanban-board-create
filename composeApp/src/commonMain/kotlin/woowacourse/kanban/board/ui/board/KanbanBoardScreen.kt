d6c6098851c3715948d9ce83a21a2afdf656ee2b
baekcii <bsy8470@naver.com>
2026-03-23 16:33:01 +0900

    feat: kanbanBoardScreen 프리뷰 정의

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/board/KanbanBoardScreen.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/ui/board/KanbanBoardScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import woowacourse.kanban.board.domain.TaskCreator
 import woowacourse.kanban.board.ui.dialog.TaskCreateDialog
 
@@ -55,4 +56,10 @@ fun KanbanBoardScreen() {
             modifier = Modifier.align(Alignment.BottomCenter),
         )
     }
+}
+
+@Composable
+@Preview
+private fun KanbanBoardScreenPreview() {
+    KanbanBoardScreen()
 }
